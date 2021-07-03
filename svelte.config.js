a923a5fd25252113dd9cba269668849dddcb2d75
tigerxp <tiger_xp@ukr.net>
2021-07-04 00:53:48 +0300

    Working bulma setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 /** @type {import('@sveltejs/kit').Config} */
+import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-node';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		adapter: adapter()
+	},
+	preprocess: [
+        preprocess({
+            "postcss": true
+        })
+    ]
 };
 
 export default config;
